51501cf941b7c259def8ad3ae61ca59a6c1518d4
Tusman4ik <tusman4ik@gmail.com>
2023-10-30 15:37:01 +0300

    feat: create project

---
+++ Project/skillforgeservice/src/main/java/com/example/skillforge/SkillforgeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillforgeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillforgeserviceApplication.class, args);
+	}
+
+}
---
+++ Project/skillforgeservice/src/test/java/com/example/skillforge/SkillforgeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillforgeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
